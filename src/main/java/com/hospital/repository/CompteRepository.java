4060032c55ed4fad5f8a1269195b7705ca2f6c38
django123 <edougajean@gmail.com>
2020-05-23 11:07:49 +0100

    add repository

---
+++ src/main/java/com/hospital/repository/CompteRepository.java
@@ -0,0 +1,13 @@
+package com.hospital.repository;
+
+import com.hospital.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+    Compte findByUsername(String username);
+    Optional<Compte>findByPersonnal_Id(Long id);
+}
