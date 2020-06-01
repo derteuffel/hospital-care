c7bf881cf78c958d585c39b7e45713f2027559a5
edougajean@gmail.com <edougajean@gmail.com>
2020-06-01 05:43:47 +0100

    crud consultation

--- src/main/java/com/hospital/Controller/ConsultationController.java
+++ src/main/java/com/hospital/Controller/ConsultationController.java
@@ -1,11 +1,22 @@
 package com.hospital.Controller;
 
 
+import com.hospital.entities.Consultation;
+import com.hospital.entities.DosMedical;
 import com.hospital.repository.ConsultationRepository;
 import com.hospital.repository.DosMedicalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/hospital-care/consultation")
@@ -17,4 +28,70 @@ public class ConsultationController {
 
     @Autowired
     private DosMedicalRepository dosMedicalRepository;
+
+
+    @GetMapping("/lists")
+    public String findAll(Model model){
+        model.addAttribute("consultations", consultationRepository.findAll());
+        return "consultation/consultations";
+    }
+
+
+    @GetMapping("/form")
+    public String form(Model model, Long id){
+        List<DosMedical> dosMedicals = dosMedicalRepository.findAll();
+        model.addAttribute("consultation", new Consultation());
+        model.addAttribute("dos", dosMedicals);
+        return  "consultation/form";
+    }
+
+    @PostMapping("/save")
+    public String saveConsultation(@Valid Consultation consultation, Long id, HttpSession httpSession){
+        DosMedical dos = dosMedicalRepository.findById(id).get();
+        consultation.setDosMedical(dos);
+        httpSession.setAttribute("id", id);
+        consultationRepository.save(consultation);
+        return "redirect:/hospital-care/dossier-medical/"+httpSession.getAttribute("id");
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateConsultation(Model model, @PathVariable Long id){
+        Consultation consultation = consultationRepository.getOne(id);
+        model.addAttribute("consultation", consultation);
+        return "consultation/edit";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(@Valid Consultation consultation, @PathVariable("id") Long id,
+                         BindingResult bindingResult, HttpSession session, Model model){
+
+        DosMedical dos = dosMedicalRepository.getOne((Long)session.getAttribute("id"));
+        consultationRepository.save(consultation);
+        model.addAttribute("consultations", consultationRepository.findAll());
+
+        return "redirect: /hospital-care/dossier-medical/"+consultation.getId();
+
+    }
+
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Long id){
+
+        Consultation consultation = consultationRepository.findById(id).get();
+        model.addAttribute("consultation", consultation);
+        return  "consultation/detail";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteConsultation(Model model, @PathVariable("id") Long id, HttpSession session){
+        Consultation consultation = consultationRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("Invalid consultation id :" +id));
+        System.out.println("consultation id"+consultation.getId());
+        DosMedical dos = dosMedicalRepository.getOne((Long)session.getAttribute("id"));
+        consultationRepository.delete(consultation);
+        model.addAttribute("consultations", consultationRepository.findAll());
+        return "redirect: /hospital-care/dossier-medical/"+dos.getId();
+    }
+
+
 }
