78713d75c92685c1c6e989b3dd895163914d66ca
derteuffel <derteuffel0@gmail.com>
2020-06-05 09:25:24 +0100

    adding heure attribute in rdv

--- src/main/java/com/hospital/entities/Rdv.java
+++ src/main/java/com/hospital/entities/Rdv.java
@@ -14,16 +14,66 @@ public class Rdv implements Serializable {
     private Long id;
     private Date date;
     private String motif;
+    private String heure;
     private Long compteId;
     private Long personnelId;
 
     public Rdv() {
     }
 
-    public Rdv(Date date, String motif, Long compteId, Long personnelId) {
+    public Rdv(Date date, String motif, String heure, Long compteId, Long personnelId) {
         this.date = date;
         this.motif = motif;
+        this.heure = heure;
         this.compteId = compteId;
+        this.personnelId = personnelId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getMotif() {
+        return motif;
+    }
+
+    public void setMotif(String motif) {
+        this.motif = motif;
+    }
+
+    public String getHeure() {
+        return heure;
+    }
+
+    public void setHeure(String heure) {
+        this.heure = heure;
+    }
+
+    public Long getCompteId() {
+        return compteId;
+    }
+
+    public void setCompteId(Long compteId) {
+        this.compteId = compteId;
+    }
+
+    public Long getPersonnelId() {
+        return personnelId;
+    }
+
+    public void setPersonnelId(Long personnelId) {
         this.personnelId = personnelId;
     }
 }
