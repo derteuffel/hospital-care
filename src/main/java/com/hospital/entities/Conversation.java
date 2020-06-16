b658fad38204e4532b7bf26489843dbcac267bb1
derteuffel <derteuffel0@gmail.com>
2020-06-16 16:13:54 +0100

    adding conversation and message object

---
+++ src/main/java/com/hospital/entities/Conversation.java
@@ -0,0 +1,70 @@
+package com.hospital.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "conversation")
+public class Conversation implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long senderId;
+    private Long receiverId;
+
+    @JsonIgnoreProperties("conversation")
+    @OneToMany(mappedBy = "conversation")
+    @OnDelete(action= OnDeleteAction.NO_ACTION)
+    @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    private List<Message> messages;
+
+    public Conversation(Long senderId, Long receiverId) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+    }
+
+    public Conversation() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(Long senderId) {
+        this.senderId = senderId;
+    }
+
+    public Long getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(Long receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+}
